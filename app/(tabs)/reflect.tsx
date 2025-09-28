a79292199d4cabd96e30d7a9299dd51dd224f393
James Taylor <james@jtaylorweb.co.uk>
2025-09-28 19:30:01 +0100

    pages for links

---
+++ app/(tabs)/reflect.tsx
@@ -0,0 +1,32 @@
+
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Image } from 'expo-image';
+import { StyleSheet } from 'react-native';
+
+export default function Calm() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#F5D9CC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Reflect</ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+});
