d775d8c51f597156004445a7bb926dd6e2f6c8d0
James Taylor <james@jtaylorweb.co.uk>
2025-09-28 19:29:41 +0100

    add links and icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,10 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -24,12 +24,34 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="calm"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Calm',
+          tabBarIcon: ({ color }) => <Ionicons name="leaf-outline" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="reflect"
+        options={{
+          title: 'Reflect',
+          tabBarIcon: ({ color }) => <Ionicons name="checkmark-circle-outline" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="advocate"
+        options={{
+          title: 'Advocate',
+          tabBarIcon: ({ color }) => <Ionicons name="document-outline" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="grow"
+        options={{
+          title: 'Grow',
+          tabBarIcon: ({ color }) => <Ionicons name="rose-outline" size={28} color={color} />,
         }}
       />
     </Tabs>
+    
   );
 }
